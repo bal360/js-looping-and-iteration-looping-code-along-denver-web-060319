019211d06403b29d811974f75e8ec0bebbb6968c
Blake A. Long <blake.a.long@gmail.com>
2019-07-15 21:52:09 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, eventName) {
+    const thankYouArray = []
+    for (let i = 0; i < array.length; i++) {
+        thankYouArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouArray;
+}
+
+function countdown(int) {
+    while (int > -1) {
+        console.log(int);
+        int--;
+    }
+}
